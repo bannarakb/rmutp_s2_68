48cdc0c8d538a4efb71477b9f27db96fb9e09955
Bannarak Bannasan <bannarak.bns@gmail.com>
2025-08-31 17:32:34 +0700

    add tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "../generated/prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = new Hono();
-
-app.get("/", (c) => c.text("Hello World"));
+app.get("/", (c) => c.text("Hello, World!"));
+app.get("/about", (c) => {
+    return c.json({
+        message: "ฺBannarak Bannasan"
+    });
+});
+app.get("/profile", async(c) => {
+    // logic
+    const profiles = await prisma.profile.findMany();
+    return c.json(profiles);
+});
 
 export default app;
-
